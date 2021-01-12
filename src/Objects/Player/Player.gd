221b738810cc3ab50df6c310f2932eb68cbcb805
Diego <ddglobaltravel1@gmail.com>
2021-01-12 01:58:42 -0400

    Player con salto v1

--- src/Objects/Player/Player.gd
+++ src/Objects/Player/Player.gd
@@ -4,6 +4,8 @@ var velocity = Vector2.ZERO
 var MAX_SPEED = 90
 var FRICTION = 500
 var ACCELERATION = 500
+var GRAVITY = 4000
+var jump_speed = -700
 var direction = "Right"
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
@@ -19,6 +21,7 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector = input_vector.normalized()
 	get_player_animation(input_vector)
+	velocity.y += GRAVITY * delta
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		animationState.travel("Run" + direction)
@@ -28,7 +31,11 @@ func _physics_process(delta):
 	move()
 
 func move():
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	#Salto basico, se debe mejorar
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = jump_speed 
 
 func get_player_animation(input_vector):
 	if input_vector.x <= -0.707:
